9c5e8319a517259bbed763e3e25b2b60631d83a2
Monika2019 <52123265+Monika2019@users.noreply.github.com>
2019-07-17 20:51:11 +0800

    lll

---
+++ machine/std_mach.py
@@ -0,0 +1,87 @@
+# -*- coding: UTF-8 -*-
+import random
+import codecs
+import os
+
+
+def create_deck_54(new_deck):
+    '推出一副新牌'
+    print('\n-- debug: I made a new deck.')
+    cardJokers = ('♞', '♘')
+    cardMarks = ('♠', '♥', '♦', '♣')
+    cardNumbers = ('2', '3', '4', '5', '6', '7', '8',
+                   '9', '10', 'J', 'Q', 'K', 'A')
+
+    for c in cardJokers:
+        new_deck.append(c)
+# print(deck)
+
+    for cn in cardNumbers:
+        for cm in cardMarks:
+            card = cm + cn
+            new_deck.append(card)
+    return
+
+
+def create_deck_52(new_deck):
+    '推出一副新牌'
+    print('\n-- debug: I made a new deck.')
+    '''
+    cardJokers = ('♞', '♘')
+    '''
+    cardMarks = ('♠', '♥', '♦', '♣')
+    cardNumbers = ('2', '3', '4', '5', '6', '7', '8',
+                   '9', '10', 'J', 'Q', 'K', 'A')
+
+    for cn in cardNumbers:
+        for cm in cardMarks:
+            card = cm + cn
+            new_deck.append(card)
+    return
+
+
+def shuffled_deck(deck_to_be_shuffled):
+    '洗牌'
+    print('\n--debug:I shuffled a deck')
+    random.shuffle(deck_to_be_shuffled)
+    return
+
+
+def record_deck(deck_to_be_record, filename):
+    '记录一副牌'
+    print('\n--debug: I record a deck')
+    out_path = os.getcwd() + '\\OutputDecks\\' + filename
+    print('--debug: %s' % (out_path))
+    f = codecs.open(out_path, 'w', 'utf-8')
+    for card in deck_to_be_record:
+        f.write(card)
+        f.write('\t')
+    f.close
+
+    return
+
+
+def make_deck_by_type(play_type, out_deck):
+    if play_type == 1:
+        create_deck_54(out_deck)
+        shuffled_deck(out_deck)
+        record_deck(out_deck, '争上游-刚洗好的牌.txt')
+    if play_type == 2:
+        create_deck_52(out_deck)
+        shuffled_deck(out_deck)
+        record_deck(out_deck, '桥牌-刚洗好的牌.txt')
+    if play_type == 3:
+        create_deck_54(out_deck)
+        shuffled_deck(out_deck)
+        record_deck(out_deck, '三人斗地主-刚洗好的牌.txt')
+    if play_type == 4:
+        deck_a = []
+        create_deck_54(deck_a)
+        out_deck.extend(deck_a)
+        deck_b = []
+        create_deck_54(deck_b)
+        out_deck.extend(deck_b)
+        shuffled_deck(out_deck)
+        record_deck(out_deck, '四人斗地主-刚洗好的牌.txt')
+
+        return
